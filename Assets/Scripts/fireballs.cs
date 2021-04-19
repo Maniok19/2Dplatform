aacdf34f0abecfd7165187812a7084e3d00fdbaf
Maniok19 <mano19029@hotmail.fr>
2021-04-19 16:27:20 +0200

    tir enemi
    ajout des boules de feu
    galere avec l'animator

---
+++ Assets/Scripts/fireballs.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireballs : MonoBehaviour
+
+{
+    public float speed;
+    public float lifeTime;
+    public float distance;
+    public Rigidbody2D rb;
+    public LayerMask whatIsSolid;
+    public int damage;
+
+    public GameObject destroyEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+        //mouvement 
+        rb.velocity = transform.right * speed;
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("player"))
+            {
+                Debug.Log("player must take dmg");
+            }
+            DestroyProjectile();
+        }
+
+
+    }
+    void DestroyProjectile()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
+
--- Assets/enemy1.cs
+++ Assets/enemy1.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class enemy1 : MonoBehaviour
 {
     public int health;
+    public GameObject fireball;
+    public Transform enemyshotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
 
 
     private void Update()
     {
+        if (timeBtwShots <= 0)
+        {
+            
+            {
+                Instantiate(fireball, enemyshotPoint.position, enemyshotPoint.rotation);
+                Debug.Log("boss must shoot");
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+
         if (health <= 0)
         {
             Destroy(gameObject);
